f1c6fa6607386e38a3ed6332b79dc92726520ced
Cristian Mihok <cristim_00@yahoo.com>
2013-07-09 15:29:18 +0300

    Delete 1.0

--- YonderTODO.js
+++ YonderTODO.js
@@ -1,18 +1,38 @@
 
 var listTodo = document.getElementById('list');
-var addBtn = document.getElementById('btn');	
+var delBtn = document.getElementById('btn');	
 var inputTodo = document.getElementById('newTodo');
 var i = 0;
 
+function createListItem(){
+	var newListItem = document.createElement('LI');
+	var newListCheckBox = document.createElement('INPUT');
+	newListCheckBox.type = 'checkbox';
+	var newTextNode = document.createTextNode((++i) + ': ' + inputTodo.value);
+	newListItem.appendChild(newTextNode);
+	newListItem.appendChild(newListCheckBox);
+	return newListItem;
+}
+
+function createDiv(){
+	var newDiv = document.createElement('DIV');			
+	var newListItem = createListItem();	
+	newDiv.id = 'listDiv';
+	newDiv.appendChild(newListItem);
+	return newDiv;
+}
+
 var addHandler = function(e){
-	if (e.keyCode == 13){
-		var newDiv = document.createElement('DIV');			
-		var newListItem = document.createElement('LI');
-		newListItem.appendChild(document.createTextNode(inputTodo.value));	
-		newDiv.id = 'listDiv';
-		newDiv.appendChild(newListItem);	
+	if (e.keyCode == 13){		
+		var newDiv = createDiv();	
 		listTodo.appendChild(newDiv);
 	}	
 }
+
+var deleteHandler = function(e){
+	var test = document.getElementById('list');
+	test.removeChild(test.firstChild);
+}
 	
 inputTodo.addEventListener('keypress', addHandler, false);
+delBtn.addEventListener('click', deleteHandler, false);
