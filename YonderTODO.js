782c5f821e4f556c354c842c66368a44f98aad7b
Cristian Mihok <cristim_00@yahoo.com>
2013-07-10 12:41:55 +0300

    Delete + Selections

--- YonderTODO.js
+++ YonderTODO.js
@@ -1,14 +1,19 @@
 
 var listTodo = document.getElementById('list');
 var delBtn = document.getElementById('btn');	
+var selAllBtn = document.getElementById('btn1');
 var inputTodo = document.getElementById('newTodo');
-var i = 0;
+var bottomListItem = document.getElementById('bottomListItem');
+var remainingTxt = document.getElementById('remainingTxt');
+
+var selectState = false;
 
 function createListItem(){
 	var newListItem = document.createElement('LI');
 	var newListCheckBox = document.createElement('INPUT');
 	newListCheckBox.type = 'checkbox';
-	var newTextNode = document.createTextNode('  ' + inputTodo.value);
+	newListCheckBox.id = 'listCB';
+	var newTextNode = document.createTextNode(inputTodo.value);
 	newListItem.appendChild(newListCheckBox);
 	newListItem.appendChild(newTextNode);
 	return newListItem;
@@ -22,36 +27,71 @@ function createDiv(){
 	return newDiv;
 }
 
-var addHandler = function(e){
-	if (inputTodo.value){
-		if (e.keyCode == 13){		
-			var newDiv = createDiv();	
-			listTodo.appendChild(newDiv);
-			newTodo.value = "";
-		}	
-	}
-	
-}
-
-function getSelectedIndexList () {
+function getSelectedItemsList () {
 	var selectedItems = new Array();
 	for (var i = 0; i < listTodo.children.length; i++) {
 		if (listTodo.children[i].children[0].children[0].checked){
 			selectedItems.push(listTodo.children[i]);
 		}		
-		//console.log(listTodo.children[i].children[0].children[0].checked);
 	}
-	//console.log(selectedIndexes);
 	return selectedItems;		
+}
+
+function toggleBotoomLI(state){
+	bottomListItem.style.visibility = state;
+}
+
+function getRemainingItems(){
+	var remainingItems = listTodo.children.length;
+	return remainingItems;
+}
+
+
+var addHandler = function(e){
+	if (inputTodo.value){
+		if (e.keyCode == 13){
+			toggleBotoomLI('visible');		
+			var newDiv = createDiv();	
+			listTodo.appendChild(newDiv);
+			newTodo.value = "";
+		}	
+	}
+	remainingItemsHandler();
+	
 }
 
 var deleteHandler = function(e){
 	var i;
-	var selectedItems = getSelectedIndexList();
+	var selectedItems = getSelectedItemsList();
 	for (i = 0; i < selectedItems.length; ++i){
 		listTodo.removeChild(selectedItems[i]);
 	}
+	if (!listTodo.children.length){
+		toggleBotoomLI('hidden');
+		newTodo.value = "";
+	}
+	remainingItemsHandler();
 }
+
+var selectAllHandler = function (e) {
+	if (selectState){
+		selectState = false;
+	}else{
+		selectState = true;
+	}
+	for (var i = 0; i < listTodo.children.length; i++) {
+		listTodo.children[i].children[0].children[0].checked = selectState;				
+	}
+}
+
+var remainingItemsHandler = function (e) {
+	remainingTxt.innerHTML = 'remaining: ' + getRemainingItems();
+}
+
+
+
+
 	
 inputTodo.addEventListener('keypress', addHandler, false);
 delBtn.addEventListener('click', deleteHandler, false);
+selAllBtn.addEventListener('click', selectAllHandler, false);
